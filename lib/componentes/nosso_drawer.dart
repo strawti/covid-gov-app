914a80975122a40366b2bb8ae8bd8fd3c816749e
Coffee <carloscoffeepy@gmail.com>
2020-04-20 19:50:34 -0300

    logo alterada, tela sobre implementada

--- lib/componentes/nosso_drawer.dart
+++ lib/componentes/nosso_drawer.dart
@@ -12,114 +12,84 @@ Widget nossoDrawer(context) {
 
     // Lista para os itens do Drawer
     child: ListView(children: <Widget>[
-      Column(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-
-              // Cabeçalho do Drawer
-              DrawerHeader(
-                child: Container(
-                  child: Center(
-                    child: Image.asset('images/logo-app.png'),
-                  ),
-                ),
-              ),
-              
-              // Abaixo está os Cards do Drawer
-              Card(
-                color: Colors.blue[200],
-                child: ListTile(
-                  leading: const Icon(Icons.home),
-                  title: const Text("Inicio"),
-                  subtitle: const Text("Home"),
-                  trailing: const Icon(Icons.arrow_forward),
-                  onTap: () {
-                    // Método para ir para a página Home
-                    Navigator.of(context).pop();
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-                  }),
-              ),
-              Card(
-                color: Colors.orange[100],
-                child: ListTile(
-                    leading: const Icon(Icons.book),
-                    title: const Text("O que é coronavírus?"),
-                    subtitle: const Text("E outros detalhes"),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.of(context).pop();
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => About()));
-                    }),
-              ),
-              Card(
-                color: Colors.green[200],
-                child: ListTile(
-                    leading: const Icon(Icons.swap_vertical_circle),
-                    title: const Text("Prevenção"),
-                    subtitle: const Text("Dicas de como prevenir"),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.of(context).pop();
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => Prevencao()));
-                    }),
-              ),
-              Card(
-                color: Colors.purple[100],
-                child: ListTile(
-                    leading: const Icon(Icons.sentiment_very_satisfied),
-                    title: const Text("Sobre esse Aplicativo"),
-                    subtitle: const Text("Detalhes sobre esse App."),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () {
-                      Navigator.of(context).pop();
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SobreApp()));
-                    }),
-              ),
-              Card(
-                color: Colors.yellow[100],
-                child: ListTile(
-                    leading: const Icon(Icons.security),
-                    title: const Text("Juntos combateremos!"),
-                    subtitle: const Text("Proteja sua família. "),
-                    trailing: const Icon(Icons.arrow_forward),
-                    onTap: () {
-                      
-                    }),
-              ),
-            ],
+      // Cabeçalho do Drawer
+      DrawerHeader(
+        child: Container(
+          child: Center(
+            child: Image.asset('images/logo-app.png'),
           ),
-
-          Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-
-                // Logo da FTeam code 
-                Container(
-                  height: 250,
-                  child: Image.asset(
-                    "images/fteamcode.jpg",
-                    semanticLabel: "Logo FTeam Code",
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-
-                // Linha para mostrar o endereço de email
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text(
-                        "E-mail: fteamcodeapps@gmail.com",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                  ],
-                ),
-              ],
+        ),
+      ),
+      
+      // Abaixo está os Cards do Drawer
+      Card(
+        color: Colors.blue[200],
+        child: ListTile(
+          leading: const Icon(Icons.home),
+          title: const Text("Inicio"),
+          subtitle: const Text("Home"),
+          trailing: const Icon(Icons.arrow_forward),
+          onTap: () {
+            // Método para ir para a página Home
+            Navigator.of(context).pop();
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+          }),
+      ),
+      Card(
+        color: Colors.orange[100],
+        child: ListTile(
+            leading: const Icon(Icons.book),
+            title: const Text("O que é coronavírus?"),
+            subtitle: const Text("E outros detalhes"),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.push(context, MaterialPageRoute(builder: (context) => About()));
+            }),
+      ),
+      Card(
+        color: Colors.green[200],
+        child: ListTile(
+            leading: const Icon(Icons.swap_vertical_circle),
+            title: const Text("Prevenção"),
+            subtitle: const Text("Dicas de como prevenir"),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Prevencao()));
+            }),
+      ),
+      Card(
+        color: Colors.purple[100],
+        child: ListTile(
+            leading: const Icon(Icons.sentiment_very_satisfied),
+            title: const Text("Sobre esse Aplicativo"),
+            subtitle: const Text("Detalhes sobre esse App."),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SobreApp()));
+            }),
+      ),
+      SizedBox(height: MediaQuery.of(context).size.height / 5),
+      // Logo da FTeam code.
+      Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: <Widget> [
+            Image.asset(
+              "images/fteamcode.jpeg",
+              semanticLabel: "Logo FTeam Code",
+              width: MediaQuery.of(context).size.width / 1.5,
             ),
-          ),
-        ],
+
+            // Linha para mostrar o endereço de email
+            const Text(
+              "E-mail: fteamcodeapps@gmail.com",
+              style: TextStyle(fontSize: 15),
+            ),
+          ]
+        ),
       ),
     ]));
   }
--- lib/telas/sobreAplicativo.dart
+++ lib/telas/sobreAplicativo.dart
@@ -11,13 +11,18 @@ class SobreApp extends StatelessWidget {
       // Corpo do aplicativo (body)
       body: Container(
         width: MediaQuery.of(context).size.width,
-  
+        padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
 
           children: <Widget>[
-            Text("Sobre esse Aplicativo", style: TextStyle(fontSize: 20),)
+            const Text("Esse aplicativo foi desenvolvido pela equipe FTeam Code.\nÉ um aplicativo simples que puxa o site do ministério da saúde com os dados estatísticos do corona vírus Covid-19, além disso ele pega informações úteis de fontes seguras também do governo.\nA intenção é que pessoas que não sabem mexer direito na internet e pesquisar pelo site oficial do Ministério da Saúde tenha em mãos todos os dados importantes com um único clique, e fique informado com os dados oficiais sobre o status desse vírus.\nÉ um aplicativo tudo em um, feito pensando na sua saúde e na do próximo.", 
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            )
           ],
         ),
       ),
